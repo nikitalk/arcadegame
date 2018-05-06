de13f76b0d4541cc0ee90a93eb0a5e6a1e6a8f59
Nikita L. Karavaev <nikita.lk@gmail.com>
2018-05-06 14:27:10 +0300

    good

--- js/app.js
+++ js/app.js
@@ -1,9 +1,10 @@
 const cols = 11;
-const scoreid = document.querySelector('#score');
+let stop = false;
+const scoreid = document.querySelector("#score");
 let score = 0;
 // Enemies our player must avoid
 class Enemy {
-    constructor(row, speed){
+    constructor(row, speed) {
         // Variables applied to each of our instances go here,
         // we've provided one for you to get started
 
@@ -11,9 +12,9 @@ class Enemy {
         // a helper we've provided to easily load images
         this.row = getRandomInt(1, 3);
         this.x = -100;
-        this.y = this.row*83-20;
+        this.y = this.row * 83 - 20;
         this.speed = getRandomInt(7, 15);
-        this.sprite = 'images/enemy-bug.png'; 
+        this.sprite = "images/enemy-bug.png";
     }
 
     // Update the enemy's position
@@ -22,94 +23,106 @@ class Enemy {
         // You should multiply any movement by the dt parameter
         // which will ensure the game runs at the same speed for
         // all computers.
-        this.x+=this.speed;
-        if (this.x > cols*101) {
-          this.x = -100;
-          this.row = getRandomInt(1, 3);
-          this.y = this.row * 83 - 20;
-          this.speed = getRandomInt(7,15);
-        }
- 
-        if ((this.row == player.row) && (this.x > player.col * 101 - 50) && (this.x < player.col * 101 + 50)) {
-        player.sprite = "images/bam.png";         
-                setTimeout(function (){
-
-player.row = 5;
-player.col = Math.floor(cols / 2);        
-player.sprite = "images/char-pink-girl.png"; 
-        }, 1000);
+        this.x += this.speed;
+        if (this.x > cols * 101) {
+            this.x = -100;
+            this.row = getRandomInt(1, 3);
+            this.y = this.row * 83 - 20;
+            this.speed = getRandomInt(7, 15);
         }
 
+        if (
+            this.row == player.row &&
+            this.x > player.col * 101 - 50 &&
+            this.x < player.col * 101 + 50
+        ) {
+            player.sprite = "images/bam.png";
+            stop = true;
+            setTimeout(function () {
+                player.row = 5;
+                player.col = Math.floor(cols / 2);
+                player.sprite = "images/char-pink-girl.png";
+                stop = false;
+            }, 1000);
+        }
     }
 
     // Draw the enemy on the screen
     render() {
-        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);  
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
 }
 
 function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 // Player class
 class Player {
-    constructor(){
+    constructor() {
         this.x = 200;
         this.y = 373;
         this.row = 5;
         this.col = Math.floor(cols / 2);
-        this.sprite = "images/char-pink-girl.png"; 
+        this.sprite = "images/char-pink-girl.png";
     }
 
     // Update the player's position
-    update() {
-
-    }
+    update() { }
 
     // Draw the enemy on the screen,
     render() {
-        ctx.drawImage(Resources.get(this.sprite), this.col*101, this.row*83-20); 
+        ctx.drawImage(
+            Resources.get(this.sprite),
+            this.col * 101,
+            this.row * 83 - 20
+        );
+        /* ctx.drawImage(Resources.get("images/char-boy.png"), this.col * 101-101, this.row * 83 - 20);
+        ctx.drawImage(Resources.get("images/char-princess-girl.png"), this.col * 101 + 101, this.row * 83 - 20); */
+        /*  ctx.drawImage(Resources.get("images/char-boy.png"), this.col * 101 - 202, this.row * 83 - 20);
+         ctx.drawImage(Resources.get("images/char-princess-girl.png"), this.col * 101 + 202, this.row * 83 - 20);
+              ctx.drawImage(Resources.get("images/char-pink-girl.png"), this.col * 101 - 303, this.row * 83 - 20);
+              ctx.drawImage(Resources.get("images/char-pink-girl.png"), this.col * 101 + 303, this.row * 83 - 20);
+                        ctx.drawImage(Resources.get("images/char-pink-girl.png"), this.col * 101 - 404, this.row * 83 - 20);
+                        ctx.drawImage(Resources.get("images/char-pink-girl.png"), this.col * 101 + 404, this.row * 83 - 20);
+                                  ctx.drawImage(Resources.get("images/char-pink-girl.png"), this.col * 101 - 505, this.row * 83 - 20);
+                                  ctx.drawImage(Resources.get("images/char-pink-girl.png"), this.col * 101 + 505, this.row * 83 - 20); */
     }
 
     handleInput(key) {
-      
         if (key == "left") {
-           if (this.col>0) this.col--;
+            if (this.col > 0) this.col--;
         }
         if (key == "right") {
-           if (this.col < cols-1) this.col++;
+            if (this.col < cols - 1) this.col++;
         }
         if (key == "up") {
             if (this.row > 0) this.row--;
             if (this.row == 0) {
-                 player.sprite = "images/great.png";
-                 setTimeout(function() {
-                   player.row = 5;
-                   player.col = Math.floor(cols / 2);
-                   player.sprite = "images/char-pink-girl.png";
-                 }, 1000);
+                player.sprite = "images/great.png";
+                stop = true;
+                setTimeout(function () {
+                    player.row = 5;
+                    player.col = Math.floor(cols / 2);
+                    player.sprite = "images/char-pink-girl.png";
+                    stop = false;
+                    p;
+                }, 1000);
                 scoreid.innerHTML = "";
-score++;
-scoreid.insertAdjacentHTML("beforeend", score);
-if (score == 5) {
-    score = 0;
-allEnemies.push(new Enemy());
-    scoreid.insertAdjacentHTML("beforeend", "You win");
-}
+                score++;
+                scoreid.insertAdjacentHTML("beforeend", score);
+                if (score == 5) {
+                    score = 0;
+                    allEnemies.push(new Enemy());
+                    scoreid.insertAdjacentHTML("beforeend", "You win");
                 }
+            }
         }
         if (key == "down") {
             if (this.row < 5) this.row++;
         }
     }
 }
-
-/* const enemy = new Enemy();
-const enemy1 = new Enemy();
-const enemy2 = new Enemy();
-const enemy3 = new Enemy();
-const enemy4 = new Enemy(); */
 
 const player = new Player();
 
@@ -118,6 +131,9 @@ const allEnemies = [];
 allEnemies.push(new Enemy());
 allEnemies.push(new Enemy());
 allEnemies.push(new Enemy());
+allEnemies.push(new Enemy());
+allEnemies.push(new Enemy());
+
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
@@ -125,13 +141,15 @@ allEnemies.push(new Enemy());
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
+document.addEventListener("keyup", function (e) {
+    if (!stop) {
+        var allowedKeys = {
+            37: "left",
+            38: "up",
+            39: "right",
+            40: "down"
+        };
 
-    player.handleInput(allowedKeys[e.keyCode]);
+        player.handleInput(allowedKeys[e.keyCode]);
+    }
 });
